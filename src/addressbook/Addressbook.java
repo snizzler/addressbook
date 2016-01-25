bae9ed6590a8a9a62046818732094bfe084333ca
Karsten Krause <Karsten Krause@10.17.1.252>
2016-01-25 15:06:23 +0100

    Moin Moin

--- src/addressbook/Addressbook.java
+++ src/addressbook/Addressbook.java
@@ -10,12 +10,13 @@ package addressbook;
  * @author Thomas
  */
 public class Addressbook {
-
+	
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int moin = 0;
+        		
         System.out.println("Hello World");
     }
     
