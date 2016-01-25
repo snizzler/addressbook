0174a5e7b4ac4022d9ac886b9b2dfa5a8317c7a4
Norman <Norman@DESKTOP-6D5NRFP.fritz.box>
2016-01-25 19:35:49 +0100

    Holla

---
+++ src/addressbook/gesammt_uebersicht.java
@@ -0,0 +1,28 @@
+package addressbook;
+
+import java.awt.*;
+
+public class gesammt_uebersicht {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Frame Fenster = new Frame("Adressbuch")
+		 
+	}
+
+}
+class Frame {
+	Frame ( string title);
+	void setSize (int widht int height); 
+	void setLocation( int x int y);
+	void setLayout (GridLayout layoutManager);
+	void add(Scrollbar comp);
+	void setViseble( boolean visible);
+}
+
+class Panel{
+	Panel ()
+	void setLayout(GridLayout layoutManager)
+	void add (Textfield comp)
+	
+}
